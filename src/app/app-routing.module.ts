b158e26b3909ff25d5f897ae80d862017b8efd24
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2019-02-18 16:55:42 +0000

    Routing, navbar and bootstrap costumization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PlaylistComponent } from './components/playlist/playlist.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'playlist/:id', component: PlaylistComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
+  exports: [RouterModule],
   imports: [
-    CommonModule
-  ],
-  declarations: []
+    RouterModule.forRoot(routes)
+  ]
 })
 export class AppRoutingModule { }
