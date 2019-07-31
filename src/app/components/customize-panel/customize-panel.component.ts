f98dab34d222457786096f703659f33ae8af34c2
Pedro Castro <p.castro@intrasurance.nl>
2019-07-31 15:14:07 +0100

    Saved bind of customize-panel inputs to ts variables. Dynamic fill of options. Imported loadash to use _.range() method

--- src/app/components/customize-panel/customize-panel.component.ts
+++ src/app/components/customize-panel/customize-panel.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from "lodash";
+
+const MAX_NUMBER_OF_ARTISTS = 5;
+const MAX_NUMBER_OF_SONGS_PER_ARTIST = 5;
 
 @Component({
   selector: 'app-customize-panel',
@@ -7,13 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomizePanelComponent implements OnInit {
   slideToggleButtonColor: string = "primary";
+  artistsNumber: number = 1;
+  songsNumber: number = 1;
+  numberOfArtistsArray: number[] = [];
+  numberOfSongsPerArtistArray: number[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.numberOfArtistsArray = _.range(1, MAX_NUMBER_OF_ARTISTS + 1);
+    this.numberOfSongsPerArtistArray = _.range(1, MAX_NUMBER_OF_SONGS_PER_ARTIST + 1);
   }
 
-  onSlideToggleChange() {
-    console.log("Toggled");
+  onArtistsNumberChange() {
+    console.log(this.artistsNumber);
+  }
+
+  onSongsNumberChange() {
+    console.log(this.songsNumber);
   }
 }
